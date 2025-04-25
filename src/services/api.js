14f403ecb9df601ba340690d72a3cd2b5047aee9
devsujal2025 <sujalb2004@gmail.com>
2025-04-25 12:49:03 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://dynamicstock-backend.onrender.com/api/auth";
 
 export const loginUser = async (email, password) => {
   return axios.post(`${API_URL}/auth/login`, { email, password });
